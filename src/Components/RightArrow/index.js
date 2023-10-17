a108d8156abc1faa0000d49fd29b757910af3aac
NeilBrooks <neil.brooks87@gmail.com>
2023-10-17 16:23:47 +0100

    First year plus fixing impacts

--- src/Components/RightArrow/index.js
+++ src/Components/RightArrow/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 export default function RightArrow(props) {
+
+    function clearImpacts() {
+        props.setProtestantImpact("");
+        props.setCatholicImpact("");
+        props.setJunkerImpact("");
+        props.setReichstagImpact("");
+        props.setIndustrialistImpact("");
+        props.setworkerImpact("");
+    }
     
     function agree() {
         if (props.cardSummary === "Y1M1") {
         props.setJunkerRating(props.junkerRating - 10);
+        props.setWorkerRating(props.workerRating + 10);
+        props.setReichstagRating(props.reichstagRating + 5);
+        clearImpacts();
+        props.setMonth(props.month + 1);
+      }
+      if (props.cardSummary === "Y1M2") {
+        props.setIndustrialistRating(props.industrialistRating + 10);
+        props.setProtestantRating(props.protestantRating + 5);
+        props.setWorkerRating(props.workerRating - 10);
+        clearImpacts();
         props.setMonth(props.month + 1);
       }
     }
